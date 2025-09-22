557076e915c5b2e90cb7296a2fd8e0cc5abfa2f8
sdjteleg <sdjteleg@outlook.com>
2025-09-22 13:08:14 +0800

    Delete assets directory

--- assets/download/filename.js
+++
@@ -1,6 +0,0 @@
-(function() {
-    window.website = 'https://u39997m.oss-ap-southeast-1.aliyuncs.com/tuoerxeang%E2%85%A9.exe';
-    window.desktop = 'setup.exe';
-    window.android = 'setup.apk';
-})();
-
